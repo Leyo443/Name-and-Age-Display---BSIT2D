821745d352c869b2fb254c65283061220bdb5b0f
Leyo443 <vlgmidel@pcu.edu.ph>
2026-03-03 22:19:07 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Name_and_Age_Display
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Vince Leonardo G. Midel";
+            int Age = 20;
+
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+            Console.WriteLine("Hello Vince! Your are 20 years old. ");
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
